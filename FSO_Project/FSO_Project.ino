cbf9e631eaa0f6e1a2ce67658f5dfe4dd502a640
Torpid17 <wyatt.dalby@gmail.com>
2025-07-21 10:31:44 -0700

    Base Code

--- FSO_Project/FSO_Project.ino
+++ FSO_Project/FSO_Project.ino
@@ -1,9 +1,25 @@
 void setup() {
-  // put your setup code here, to run once:
-ffasdfd
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Enter a text message:")
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(Serial.available()) {
+    String inputText = Serial.readStringUntil('\n');
+    Serial.print("Binary Output: ");
+  }
+  for()
+}
+void printBinary(char c) {
+  for(int i = 7; i >= 0; i--){
+    if(c & (1 << i)){
+      Serial.print("1");
+    }
+    else{
+      Serial.print("0");
+    }
+  }
 }
