bce7b8d4f7a015c5c77aeea31639762e110ee6a6
Torpid17 <wyatt.dalby@gmail.com>
2025-07-22 10:30:36 -0700

    Upadated

--- FSO_Project/FSO_Project.ino
+++ FSO_Project/FSO_Project.ino
@@ -1,23 +1,27 @@
 #
 int sendSpeed = 100;
+int transmitter = D2;
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT); 
-  pinMode(D2, OUTPUT);
+  pinMode(transmitter, OUTPUT);
   Serial.begin(9600);
   while(!Serial);
   Serial.println("Enter a text message:");
 
 }
 
+
 void loop() {
   // put your main code here, to run repeatedly:
   if(Serial.available()) {
     String inputText = Serial.readStringUntil('\n');
-    Serial.print("Binary Output: ");
+    startBit();
+    Serial.print("Transmitting Binary...\nBinary Output: ");
     
     for(int i = 0; i < inputText.length(); i++){
       char c = inputText[i];
-      printBinary(c);
+      sendBinary(c);
       Serial.print(" ");
       }
     Serial.println();
@@ -25,7 +29,9 @@ void loop() {
   }
 }
 
-void printBinary(char c) {
+
+
+void sendBinary(char c) {
   for(int i = 7; i >= 0; i--){
     if(c & (1 << i)){
       Serial.print("1");
@@ -42,12 +48,17 @@ void printBinary(char c) {
 
 void sendBit(int x){
   if(x == 1){
-    digitalWrite(D2, HIGH)
+    digitalWrite(transmitter, HIGH)
   }
   else{
-    digitalWrite(D2, LOW)
+    digitalWrite(transmitter, LOW)
   }
   delay(sendSpeed);
+}
+
+void startBit(){
+  digitalWrite(transmitter, HIGH);
+  delay(500);
 }
 
 
--- FSO_Project/FSO_Receiver/FSO_Receiver.ino
+++ FSO_Project/FSO_Receiver/FSO_Receiver.ino
@@ -1,9 +1,48 @@
+
+int recSpeed = 100;
+int receiver = A1;
+int threshold = 100;
+int bitCount = 0;
+String binaryInput = "";
+
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode
 
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(bitStart()){
+    getBit()
+  }
+}
 
+void binarytoText(){
+
+}
+
+boolean bitStart(){
+  for(int i = 0; i < 50; i++){
+    if(analogRead(receiver) < threshold){
+      return false;
+    }
+    delay(10);
+  }
+  return true;
+}
+
+String processWord(){
+
+}
+void getBit(){
+  if(analogRead(receiver) >= threshold){
+    binaryInput += "1";
+  }
+  else{
+    binaryInput += "0";
+  }
+  
 }
