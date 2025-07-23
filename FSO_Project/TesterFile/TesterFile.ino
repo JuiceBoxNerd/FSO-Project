590f9dd632f68908d364ef28c71452a304765e30
Torpid17 <wyatt.dalby@gmail.com>
2025-07-23 14:56:02 -0700

    Update TesterFile.ino

--- FSO_Project/TesterFile/TesterFile.ino
+++ FSO_Project/TesterFile/TesterFile.ino
@@ -2,12 +2,14 @@ String binaryInput = "";
 
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+  while(!Serial);
 }
 
 void loop() {
   // put your main  code here, to run repeatedly:
-  Serial.begin(9600)
+  digitalWrite(2, HIGH);
 }
 
 String binaryToText(){
@@ -15,7 +17,7 @@ String binaryToText(){
   int len = binaryInput.length();
   for(int i = 0; i < len; i += 8){
     String letter = binaryInput.substring(i, i+8);
-    char c = strtol(letter.cstr(), NULL, 2);
+    char c = strtol(letter.c_str(), NULL, 2);
     Serial.print(c);
     text += c;
   }
